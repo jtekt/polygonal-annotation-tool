2b6556e6ecec4923eaffd110649118f3c4fb9f73
Maxime Moreillon <m.moreillon@gmail.com>
2023-03-28 16:10:36 +0900

    default annotation filed

--- src/store/index.js
+++ src/store/index.js
@@ -4,12 +4,12 @@ import Vuex from "vuex"
 Vue.use(Vuex)
 
 // FIXME: does not default to "annotation" when env var is not set
-const { VUE_APP_ANNOTATION_FIELD = "annotation" } = process.env
+const { VUE_APP_ANNOTATION_FIELD } = process.env
 
 export default new Vuex.Store({
   state: {
     current_user: null,
-    annotation_field: VUE_APP_ANNOTATION_FIELD,
+    annotation_field: VUE_APP_ANNOTATION_FIELD || "annotation",
   },
   mutations: {
     set_current_user(state, user) {
