72c4f730772ccad0a08fda516c2fa77c77ddd732
Maxime Moreillon <m.moreillon@gmail.com>
2023-08-08 08:04:38 +0900

    template update

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,20 +1,22 @@
-import Vue from 'vue';
-import Vuetify from 'vuetify/lib/framework';
+import Vue from 'vue'
+import Vuetify from 'vuetify/lib/framework'
 
 import '@mdi/font/css/materialdesignicons.css' // Ensure you are using css-loader
 
-Vue.use(Vuetify);
+Vue.use(Vuetify)
 
 export default new Vuetify({
-  icons: {
-    iconfont: 'mdi', // default - only for display purposes
-  },
-  theme: {
-    themes: {
-      light: {
-        primary: '#c00000',
-
-      },
+    icons: {
+        iconfont: 'mdi', // default - only for display purposes
     },
-  },
+    theme: {
+        themes: {
+            light: {
+                primary: '#c00000',
+            },
+            dark: {
+                primary: '#c00000',
+            },
+        },
+    },
 })
